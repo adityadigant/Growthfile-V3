2f583e851318a191e81015199e3631c976e78f02
Aditya Digant <adityadigant96@yahoo.com>
2020-10-13 22:54:08 +0530

    Validation on Required Fields
    Applied Validation for IFSC, PAN and AADHAAR Numbers inputs.

--- public/js/profile_aadhaar.js
+++ public/js/profile_aadhaar.js
@@ -13,3 +13,17 @@ var load_front = function(event) {
     image_back.style.height= "206px";
     image_back.style.margin="0";
   };
+
+  function isPossiblyValidAadharNumber(string) {
+    return /^\d{4}\d{4}\d{4}$/.test(string)
+  }
+  
+function aadhaar_validation() {
+      var aadharNumber = document.getElementById("aadhaar_number").value;
+      var alert_text = document.getElementById("alert_message");
+      if (!isPossiblyValidAadharNumber(aadharNumber.trim())) {
+        alert_text.style.display="block";
+        return;
+      }else{
+        alert_text.style.display="none";
+      }}
--- public/js/profile_pan.js
+++ public/js/profile_pan.js
@@ -6,3 +6,20 @@ var load_front = function(event) {
     image.style.margin="0";
     
   };
+
+  function isPossiblyValidPan(string) {
+    return /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/.test(string)
+  }
+
+    
+function pan_validation() {
+  var panNumber = document.getElementById("pan_card").value;
+  var alert_text = document.getElementById("alert_message");
+  if (!isPossiblyValidPan(panNumber.trim())) {
+    alert_text.style.display="block";
+        return;
+      }else{
+        alert_text.style.display="none";
+  }
+  }
+  
