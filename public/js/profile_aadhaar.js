3255eaabe46086d664fc2f009a97892e51fb5ab8
Aditya Digant <adityadigant96@yahoo.com>
2020-10-13 14:36:05 +0530

    Full Aadhar and PAN Cover

--- public/js/profile_aadhaar.js
+++ public/js/profile_aadhaar.js
@@ -1,9 +1,15 @@
 var load_front = function(event) {
     var image_front = document.getElementById('output_front');
     image_front.src = URL.createObjectURL(event.target.files[0]);
+    image_front.style.width="100%";
+    image_front.style.height= "206px";
+    image_front.style.margin="0";
   };
 
   var load_back = function(event) {
     var image_back = document.getElementById('output_back');
     image_back.src = URL.createObjectURL(event.target.files[0]);
+    image_back.style.width="100%";
+    image_back.style.height= "206px";
+    image_back.style.margin="0";
   };
--- public/js/profile_pan.js
+++ public/js/profile_pan.js
@@ -1,4 +1,8 @@
 var load_front = function(event) {
     var image = document.getElementById('output_pan');
     image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.width="100%";
+    image.style.height= "206px";
+    image.style.margin="0";
+    
   };
